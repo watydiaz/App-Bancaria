a4fd3ba6300635ea4de92b9616f22eb8fdade892
watydiaz <40471433+watydiaz@users.noreply.github.com>
2025-03-27 19:46:15 -0500

    El profe quiere el ejercicio sin bootstrap

--- script.js
+++ script.js
@@ -17,7 +17,25 @@ clientes = [
         numeroCuenta: "1073679337",
         saldo: 5000000,
         transferencias: [],
-    }
+    },
+    {
+        usuario: "k",
+        contraseña: "1",
+        nombre: "Bruno Diaz",
+        apellido: "Diaz",
+        numeroCuenta: "1073610079337",
+        saldo: 5000000,
+        transferencias: [],
+    },
+    {
+        usuario: "Julian",
+        contraseña: "1234",
+        nombre: "Julain",
+        apellido: "Florez",
+        numeroCuenta: "45645575768578",
+        saldo: 70000000,
+        transferencias: [],
+    },
 ];
 
 // Solicitar el usuario
@@ -45,13 +63,32 @@ if (!cliente) {
         window.location = "index.html";
     } else {
         alert(`Bienvenido, ${cliente.nombre} ${cliente.apellido}`);
-        
-         // Actualizar el valor del input con el nombre del usuario
-         document.f1.nomUser.value = `${cliente.nombre} ${cliente.apellido}`;
-         // Actualizar total de la cuenta
-         document.f1.nCuenta.value = `${cliente.numeroCuenta}`;
-            
-        
-        // Aquí puedes continuar con las operaciones del cliente
+
+        // Actualizar el valor del input con el nombre del usuario
+        document.f1.nomUser.value = `${cliente.nombre} ${cliente.apellido}`;
+        // Actualizar total de la cuenta
+        document.f1.nCuenta.value = `\n\t${cliente.numeroCuenta}`;
+        // Actualizar saldo
+        // Formatear el saldo con separadores de miles para mostrarlo
+        document.f1.saldo.value = `\n\t${cliente.saldo.toLocaleString('es-ES')}`;
+
+
+        // Informacion desplegada en la caja modal de contactos:
+        document.addEventListener("DOMContentLoaded", function () {
+            // Obtener el elemento de la lista
+            listaUsuarios = document.getElementById("listaUsuarios");
+
+            // Llenar la lista de usuarios usando un bucle for
+            for (i = 0; i < clientes.length; i++) {
+                cliente = clientes[i];
+                li = document.createElement("li");
+                li.textContent = `${cliente.nombre} ${cliente.apellido} (${cliente.usuario})`;
+                li.classList.add("list-group-item");
+                listaUsuarios.appendChild(li);
+            }
+        });
     }
 }
+
+
+// Función para realizar transferencia
